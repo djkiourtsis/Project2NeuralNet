f6d79cf7070e5aecae098bb083c1edd12e22fbe8
lili <lli2@wpi.edu>
2016-09-10 19:01:02 -0400

    Skeleton ann.py and training data
    Core code for the ann.py and the
    training data that was provided.

---
+++ ann.py
@@ -0,0 +1,34 @@
+from numpy import *
+
+
+def sigmoid(input):
+    #sigmoid function
+
+
+def sigmoidDeriv(input):
+    #derivative of the sigmoid function
+
+
+def main(argv):
+    #check arguments & set defaults
+    
+    
+    #try and open file
+    #read the file into the input and output arrays two inputs and one output
+    #separate the data into the input and training data based on either the given percentage or the default value of 20%
+    
+    
+    #initialize the input and hidden layer with random weights
+    
+    
+    #loops over the training data 1000 times
+    for i in xrange(1000):
+        #set the input layer layer values form the input array
+        #forwardprop the input to the hidden layers
+        #forwardprop hidden layer outputs to the output layer
+        #calculate output layer error using the given data.
+        #calculate the output layer delta.
+        #calc the hidden layer error using the output delta value and weights.
+        #calculate the hidden layer delta by using the errors
+        #modify input weights and hidden layer weights depending on the delta values and the output of the neurons.
+        
